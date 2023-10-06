b174b6c31df6d6db5c052f94572aa02306d4a96d
hiropy <126783940+hirohiro-sys@users.noreply.github.com>
2023-10-06 07:18:45 +0000

    letとconst

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,37 @@
-import "./styles.css";
+// /**const,let */
+// var val1 = "var変数"
+// console.log(val1)
+// val1 = "上書き"
+// console.log(val1)
+// var val1 = "varを再設定"
+// console.log(val1)
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//  varは上書き再設定可能
+
+// let val2 = "let変数"
+// console.log(val2)
+// val2 = "上書き"
+// console.log(val2)
+// let val2 = "再宣言"
+// letは上書きができるけど再宣言できない
+
+// const  val3 = "const変数";
+// console.log(val3);
+// val3 = "上書き";
+// const val3 = "再宣言";
+//constは上書きも再宣言もできない
+//constのオブジェクトはプロパティの変更追加可能
+// const val4 = {
+//     name : "ひろ",
+//     age :"21",
+// };
+// console.log(val4);
+// val4.name = "jak";
+// val4.addres = "千葉";
+// console.log(val4);
+
+//constの配列も変更や追加可能
+// const val5 = ["dog", "cat"];
+// val[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
